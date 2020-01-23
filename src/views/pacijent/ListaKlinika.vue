3b0bf1a15ca505c3c9d88ec93de8716345b17bb4
DamjanPantic <panticdamjan@gmail.com>
2020-01-23 21:11:31 +0100

    feature/1-8: odradjena pretraga po tipu pregleda i zapoceto po datumu

--- src/views/pacijent/ListaKlinika.vue
+++ src/views/pacijent/ListaKlinika.vue
@@ -8,6 +8,14 @@
         </div>
       </div>
 
+      <b-container v-if="errorFlag">
+          <b-alert
+            show=""
+            variant="danger"
+            class="d-flex justify-content-center mt-2"
+          >Kod pretrage morate uneti datum pregleda kao i tip pregleda</b-alert>
+        </b-container>
+
       <b-container class="mt-4">
         <h4>Sortiraj po</h4>
         <b-select v-model="sortirajPo">
@@ -17,19 +25,61 @@
         </b-select>
       </b-container>
 
+      <b-container class="mt-4">
+        <div class="row">
+          <div class="col">
+            <button
+              type="button"
+              class="btn btn-outline-secondary btn-block z-depth-2"
+              @click="prikaziPretragu"
+            >{{pretraziBtnClickerd? "Ukloni":"Prikazi"}} formu za pretragu</button>
+          </div>
+        </div>
+      </b-container>
+
+      <b-container class="mt-4" v-if="pretraziBtnClickerd">
+        <div class="row">
+          <div class="col">
+            <h4>Datum</h4>
+            <b-form-group>
+              <b-form-input type="date" v-model="pretraga.datumPregleda"></b-form-input>
+            </b-form-group>
+          </div>
+          <div class="col">
+            <h4>Tip pregleda</h4>
+            <b-select v-model="pretraga.tipPregleda">
+              <option
+                v-for="tipPregleda in tipoviPregleda"
+                :key="tipPregleda.id"
+                :value="tipPregleda.id"
+              >{{tipPregleda.naziv}}</option>
+            </b-select>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <button
+              type="button"
+              class="btn btn-secondary btn-block z-depth-2"
+              @click="pretrazi"
+            >Pretrazi</button>
+          </div>
+        </div>
+      </b-container>
+
       <b-container class="mt-4 mb-2">
         <div class="row">
           <div class="col">
             <h5>Drzava</h5>
             <b-select v-model="selektovanaDrzava">
-              <option value="">-</option>
+              <option value>-</option>
               <option v-for="drzava in drzave" :key="drzava" :value="drzava">{{drzava}}</option>
             </b-select>
           </div>
           <div class="col">
             <h5>Grad</h5>
             <b-select v-model="selektovaniGrad">
-              <option value="">-</option>
+              <option value>-</option>
               <option v-for="grad in gradovi" :key="grad" :value="grad">{{grad}}</option>
             </b-select>
           </div>
@@ -50,8 +100,8 @@
         <div class="row">
           <div class="col">
             <div class="md-form">
-              <label>Opis</label>
-              <label class="form-control">{{klinika.opis}}</label>
+              <label>Adresa</label>
+              <label class="form-control">{{klinika.adresa}}</label>
             </div>
           </div>
           <div class="col">
@@ -77,21 +127,72 @@ export default {
       drzave: [],
       gradovi: [],
       selektovanaDrzava: "",
-      selektovaniGrad: ""
+      selektovaniGrad: "",
+      pretraziBtnClickerd: false,
+      tipoviPregleda: [],
+      pretraga: {
+        datumPregleda: "",
+        tipPregleda: "",
+      },
+      errorFlag: false,
     };
   },
   methods: {
     compare(a, b) {
-      if (a[this.sortirajPo] < b[this.sortirajPo]) {
-        return -1;
-      }
-      if (a[this.sortirajPo] > b[this.sortirajPo]) {
-        return 1;
+      if (this.sortirajPo === "naziv") {
+        if (a[this.sortirajPo] < b[this.sortirajPo]) {
+          return -1;
+        }
+        if (a[this.sortirajPo] > b[this.sortirajPo]) {
+          return 1;
+        }
+      }else{
+        if (a[this.sortirajPo] < b[this.sortirajPo]) {
+          return 1;
+        }
+        if (a[this.sortirajPo] > b[this.sortirajPo]) {
+          return -1;
+        }
       }
       return 0;
     },
     otvoriKliniku(klinika) {
       this.$router.push("/klinika/" + klinika.id + "/" + klinika.naziv);
+    },
+    prikaziPretragu() {
+      if (!this.pretraziBtnClickerd) {
+        axios
+          .get("/tipPregleda/sviTipovi")
+          .then(tipoviPregleda => {
+            this.tipoviPregleda = tipoviPregleda.data;
+            this.pretraziBtnClickerd = !this.pretraziBtnClickerd;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      } else {
+        this.pretraziBtnClickerd = !this.pretraziBtnClickerd;
+        this.pretraga.datumPregleda = "";
+        this.pretraga.tipPregleda = "";
+        this.errorFlag = false;
+      }
+    },
+    pretrazi() {
+      this.errorFlag = false;
+
+      if (this.pretraga.datumPregleda === "" && this.pretraga.tipPregleda === "") {
+        this.errorFlag = true;
+      }
+
+      axios
+          .post("/klinika/pretraziKlinike/"+this.pretraga.datumPregleda+"/"+this.pretraga.tipPregleda)
+          .then(klinike => {
+            this.klinike = klinike.data;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      
     }
   },
   computed: {
@@ -100,18 +201,21 @@ export default {
 
       if (this.selektovanaDrzava === "" && this.selektovaniGrad === "") {
         klinike = this.klinike.sort(this.compare);
-      }else{
+      } else {
         this.klinike.sort(this.compare).forEach(klinika => {
-        if (this.selektovanaDrzava === klinika.drzava) {
-          if (this.selektovaniGrad === "" ||  this.selektovaniGrad === klinika.grad) {
-            klinike.push(klinika);
+          if (this.selektovanaDrzava === klinika.drzava) {
+            if (
+              this.selektovaniGrad === "" ||
+              this.selektovaniGrad === klinika.grad
+            ) {
+              klinike.push(klinika);
+            }
+          } else if (this.selektovanaDrzava === "") {
+            if (this.selektovaniGrad === klinika.grad) {
+              klinike.push(klinika);
+            }
           }
-        }else if (this.selektovanaDrzava === "") {
-          if (this.selektovaniGrad === klinika.grad) {
-            klinike.push(klinika);
-          }
-        }
-      });
+        });
       }
 
       return klinike;
@@ -122,7 +226,7 @@ export default {
       this.selektovaniGrad = "";
 
       if (!novaDrzava) {
-        novaDrzava = "none"
+        novaDrzava = "none";
       }
 
       axios
