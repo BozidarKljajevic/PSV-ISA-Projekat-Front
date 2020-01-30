9086110bc4ddb19c508c0941bfccb598f80859d5
BozidarKljajevic <bozidar.kljajevic07@gmail.com>
2020-01-30 14:57:43 +0100

    Commit za spajanje sa pretragom od kolege

--- src/views/administrator_klinike/ZahteviZaOperaciju.vue
+++ src/views/administrator_klinike/ZahteviZaOperaciju.vue
@@ -1,49 +1,90 @@
 <template>
   <b-container class="mt-4">
-      <div class="col-6 mb-4" v-for="zahtev in zahtevi" :key="zahtev.id">
-        <b-card bg-variant="danger" text-variant="white" header="Pregled" class="text-center">
-          <div class="row">
-            <div class="col">
-              <div class="md-form">
-                <label>Tip Pregleda</label>
-                <label class="form-control">{{zahtev.tipPregleda.naziv}}</label>
-                <label>Datum</label>
-                <label class="form-control">{{zahtev.datum}}</label>
-              </div>
-            </div>
-            <div class="col">
-              <div class="md-form pb-3">
-                <label>Klinika</label>
-                <label class="form-control">{{zahtev.lekar.klinika.naziv}}</label>
-                <label>Vreme</label>
-                <label class="form-control">{{zahtev.vreme}}</label>
-              </div>
-            </div>
-            <div class="col">
-              <div class="md-form pb-3">
-                <label>Lekar</label>
-                <label class="form-control">{{zahtev.lekar.ime}} {{zahtev.lekar.prezime}}</label>
-                <label>Cena</label>
-                <label class="form-control">{{zahtev.cena}}</label>
-              </div>
+    <div class="col-6 mb-4" v-for="zahtev in zahtevi" :key="zahtev.id">
+      <b-card bg-variant="danger" text-variant="white" header="Pregled" class="text-center">
+        <div class="row">
+          <div class="col">
+            <div class="md-form">
+              <label>Tip Pregleda</label>
+              <label class="form-control">{{zahtev.tipPregleda.naziv}}</label>
+              <label>Datum</label>
+              <label class="form-control">{{zahtev.datum}}</label>
             </div>
           </div>
-          <!--<template v-slot:footer>
-            <b-button @click="zakazi(zahtev.id)">Zakazi</b-button>
-          </template>-->
-        </b-card>
-      
+          <div class="col">
+            <div class="md-form pb-3">
+              <label>Klinika</label>
+              <label class="form-control">{{zahtev.lekar.klinika.naziv}}</label>
+              <label>Vreme</label>
+              <label class="form-control">{{zahtev.vreme}}</label>
+            </div>
+          </div>
+          <div class="col">
+            <div class="md-form pb-3">
+              <label class="typo__label">Lekar</label>
+                <multiselect v-model="SelektovaniLekari" placeholder="Search" label="ime" track-by="ime" :options="LekariKlinike" :multiple="true"><span slot="noResult">Oops! No elements found. Consider changing the search query.</span></multiselect>
+               <!---pre class="language-json"><code>{{ SelektovaniLekari  }}</code></pre--->
+
+              <label>Cena</label>
+              <label class="form-control">{{zahtev.cena}}</label>
+            </div>
+          </div>
+        </div>
+        <div>
+          <label for="Form-klinika">Dodaj salu</label>
+                <b-form-select v-model="selektovanaSala">
+                    <option
+                      v-for="sale in SaleKlinike"
+                      :value="sale.id"
+                      :key="sale.id"
+                    >{{sale.naziv}}</option>
+                  </b-form-select>
+          
+        </div>
+        <button
+          type="button"
+          class="btn btn-success btn-block mt-2 z-depth-2"
+          @click="rezervisiSalu(zahtev)"
+        >Zakazi pregled</button>
+      </b-card>
     </div>
   </b-container>
 </template>
 
 <script>
 import axios from "axios";
+import Multiselect from 'vue-multiselect';
+
 export default {
-    data() {
+  components: {
+    Multiselect
+  },
+  data() {
     return {
-      zahtevi: []
+      zahtevi: [],
+      SaleKlinike: [],
+      selektovanaSala: "",
+      LekariKlinike: [],
+      SelektovaniLekari: []
     };
+  },
+  methods: {
+    rezervisiSalu(zahtev) {
+      axios
+        .post("/zahtevi/rezervisiSalu/" + this.selektovanaSala, zahtev)
+        .then(response => {})
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    /*addTag(newTag) {
+      const tag = {
+        ime: newTag,
+        ime: newTag.substring(0, 2) + Math.floor(Math.random() * 10000000)
+      };
+      this.LekariKlinike.push(tag);
+      this.SelektovaniLekari.push(tag);
+    }*/
   },
   mounted() {
     axios
@@ -54,8 +95,25 @@ export default {
       .catch(error => {
         console.log(error);
       });
+    axios
+      .get("/salaKLinike/SaleKlinike/" + this.$store.state.user.id)
+      .then(SaleKlinike => {
+        this.SaleKlinike = SaleKlinike.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    axios
+      .get("/lekar/postojeciLekariKlinike/" + this.$store.state.user.id)
+      .then(LekariKlinike => {
+        this.LekariKlinike = LekariKlinike.data;
+        console.log(this.LekariKlinike)
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
-}
+};
 </script>
 
 <style>
