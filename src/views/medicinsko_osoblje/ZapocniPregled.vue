131e514946affcc4d9cc4b2974c2ca7f6a90395f
BozidarKljajevic <bozidar.kljajevic07@gmail.com>
2020-02-05 00:46:12 +0100

    Uradjen front za zavrsavanje izvestaja o pregledu

--- src/views/medicinsko_osoblje/ZapocniPregled.vue
+++ src/views/medicinsko_osoblje/ZapocniPregled.vue
@@ -1,21 +1,18 @@
 <template>
   <div>
     <div>
-     
       <div class="container d-flex justify-content-center" style="margin-top: 20px">
         <div class="card" style="width: 60%">
           <div class="header pt-3 grey lighten-2">
             <div class="row d-flex justify-content-start">
-              <h3 class="deep-grey-text mt-3 mb-4 pb-1 mx-5">Pregled pacijenta </h3>
+              <h3 class="deep-grey-text mt-3 mb-4 pb-1 mx-5">Pregled pacijenta</h3>
             </div>
           </div>
-        <div class="form-group">
+          <div class="form-group">
             <div class="card-body mx-4 mt-4">
               <div class="row">
                 <div class="col">
                   <div class="md-form">
-                    
-
                     <label for="Form-ime">Ime</label>
                     <input
                       type="text"
@@ -25,41 +22,21 @@
                       v-model="user.ime"
                     />
 
-                    <label for="Form-klinika">Lekovi</label>
-                  <b-form-select >
-                    <option
-                      v-for="lek in lekovi"
-                      :key="lek.sifra"
-                    >{{lek.naziv}}</option>
-                  </b-form-select>
-            
-                 <label for="Form-klinika">Bolesti</label>
-                  <b-form-select >
-                    <option
-                      v-for="bol in bolesti"
-                      
-                      :key="bol.sifra"
-                    >{{bol.naziv}}</option>
-                  </b-form-select>
-                    
-               
-                  <button
-                    type="button"
-                    class="btn btn-danger btn-block  mt-2 z-depth-2"
-                   
-                  >Zavrsi pregled</button>
-                
-                  
-                  
-              
+                    <label for="Form-klinika">Bolest</label>
+                    <b-form-select v-model="zahtev.bolest">
+                      <option v-for="bol in bolesti" :value="bol" :key="bol.sifra">{{bol.naziv}}</option>
+                    </b-form-select>
 
-                </div>
+                    <button
+                      type="button"
+                      class="btn btn-danger btn-block mt-5 z-depth-2"
+                      @click="zavrsiPregled"
+                    >Zavrsi pregled</button>
+                  </div>
                 </div>
 
-                    <div class="col">
+                <div class="col">
                   <div class="md-form pb-3">
-                   
-
                     <label for="Form-prezime">Prezime</label>
                     <input
                       type="text"
@@ -67,72 +44,99 @@
                       class="form-control"
                       disabled
                       v-model="user.prezime"
-                      
                     />
 
-                    <label for="Form-city">Opis</label>
-                    <input
-                      type="text"
-                      id="Form-city"
-                      class="form-control"
-                      
-                    />
-
-                    <label for="Form-city">Dijagnoza</label>
-                    <input
-                      type="text"
-                      id="Form-city"
-                      class="form-control"
-                      
-                    />
-                    
-                   <router-link :to="'/ZakaziPregled/'+this.$route.params.id" tag="button" class="btn btn-success mt-2 btn-block z-depth-2">Zakazi Pregled</router-link>
-
-                    
+                    <div>
+                      <label class="typo__label">Lekovi</label>
+                      <multiselect
+                        v-model="zahtev.lekovi"
+                        :options="lekovi"
+                        :multiple="true"
+                        :close-on-select="false"
+                        :clear-on-select="false"
+                        :preserve-search="true"
+                        placeholder=" Izaberi"
+                        label="naziv"
+                        track-by="naziv"
+                        :preselect-first="true"
+                      >
+                        <template slot="selection" slot-scope="{ values, search, isOpen }">
+                          <span
+                            class="multiselect__single"
+                            v-if="values.length &amp;&amp; !isOpen"
+                          >{{ values.length }} izabran/o</span>
+                        </template>
+                      </multiselect>
+                      <!--<pre class="language-json"><code>{{ value  }}</code></pre>-->
                     </div>
+
+                    <button
+                      type="button"
+                      class="btn btn-danger btn-block mt-2 z-depth-2"
+                    >Prosledi recept</button>
+
+                    <router-link
+                      :to="'/ZakaziPregled/'+this.$route.params.id"
+                      tag="button"
+                      class="btn btn-success mt-2 btn-block z-depth-2"
+                    >Zakazi dodatno</router-link>
                   </div>
-
-                
-
+                </div>
               </div>
-
-              
-            
-         </div>
+            </div>
+          </div>
         </div>
-    </div>
-</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Multiselect from "vue-multiselect";
 export default {
-     data() {
+  components: {
+    Multiselect
+  },
+  data() {
     return {
       user: {},
       lekovi: [],
-      bolesti: []
+      bolesti: [],
+      zahtev: {
+        pacijent: {},
+        lekar: {},
+        bolest: {},
+        lekovi: []
+      }
     };
   },
 
- mounted() {
+  mounted() {
     axios
       .get("/pacijent/preuzmiPac/" + this.$route.params.id)
       .then(pacijent => {
         this.user = pacijent.data;
+        this.zahtev.pacijent = pacijent.data;
       })
       .catch(error => {
         console.log(error);
       });
 
+    axios
+      .get("/lekar/lekarPregleda/" + this.$route.params.id)
+      .then(lekar => {
+        this.zahtev.lekar = lekar.data;
+        console.log(this.zahtev);
+      })
+      .catch(error => {
+        console.log(error);
+      });
 
-       axios
+    axios
       .get("/lek/postojeciLek")
       .then(lekovi => {
         this.lekovi = lekovi.data;
-        
       })
       .catch(error => {
         console.log(error);
@@ -141,19 +145,26 @@ export default {
       .get("/bolesti/postojeceBolesti")
       .then(bolesti => {
         this.bolesti = bolesti.data;
-        
       })
       .catch(error => {
         console.log(error);
       });
+  },
 
+  methods: {
+    zavrsiPregled() {
+
+      console.log(this.zahtev);
+
+      axios
+        .post("/izvestajpregleda/zavrsi/" + this.$route.params.id, this.zahtev)
+        .then(response => {})
+        .catch(error => {
+          console.log(error);
+        });
+    }
   }
-
-
-
-      
-}
-
+};
 </script>
 
 <style>
